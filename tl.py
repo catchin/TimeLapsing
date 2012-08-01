766cdef9ee1437c1c616e70005ef003f38f2b23a
Fabian Kneißl <fabian@kneissl-web.net>
2012-08-01 23:50:48 +0200

    converting all

--- tl.py
+++ tl.py
@@ -10,10 +10,13 @@ class TimeLapseCatchin:
 	def __init__(self):
 		self.sourceDir = "."
 
-	def convertAll(self, sourceDir, destDir, options):
-		for files in os.listdir(sourceDir):
-			if files.endswith(".jpg") or files.endswith(".jpeg"):
-				self.convertSingle(files, os.path.join(destDir, files), options)
+	def convertAll(self, options):
+		print("Converting images...")
+		files = filter(endswith(".jpg"), os.listdir(self.sourceDir))
+		for f in files:
+			self.convertSingle(f, os.path.join(self.destDir, f), options)
+			sys.stdout.write(".")
+		print("done")
 
 	def convertSingle(self, img, outimg, options):
 		args = ["convert"]
@@ -21,7 +24,7 @@ class TimeLapseCatchin:
 		args.extend([img, outimg])
 		return subprocess.call(args)
 
-	def testForConvertParameters(self, options=None):
+	def testForConvertParameters(self, img=None, options=None):
 		print("Enter the conversion you want to do.\n" \
 				"Examples:\n" \
 				" -crop 2144x1206+0+109 +repage -scale 1920x1080\n" \
@@ -30,13 +33,18 @@ class TimeLapseCatchin:
 				" -crop 3216x1809+0+50 +repage -scale 1920x1080\n\n" \
 				"Enter a blank line when you are satisfied with the output image.\n")
 
+		if img == None:
+			for files in os.listdir(self.sourceDir):
+				if files.endswith(".jpg"):
+					img = files
+					break
 		while True:
 			sys.stdout.write("=> ")
 			readline = sys.stdin.readline().strip()
 			if len(readline) == 0:
 				return options
 			options = readline.split(" ")
-			self.testConvert("20120730-n032732.jpg", options)
+			self.testConvert(img, options)
 
 	def testConvert(self, img, options):
 		tmpfile = tempfile.NamedTemporaryFile(suffix=".jpg")
@@ -47,7 +55,10 @@ class TimeLapseCatchin:
 	
 	def run(self):
 		options = self.testForConvertParameters()
-		print(options)
+		self.convertAll()
+		self.destDir = os.path.join(self.sourceDir, "converted")
+		os.mkdir(self.destDir)
+		convertAll(options)
 
 
 parser = argparse.ArgumentParser()
