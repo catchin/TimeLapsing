c970281c91f8fbd46590c47817f54bb354734e3c
Fabian Kneißl <fabian@kneissl-web.net>
2012-08-01 23:42:51 +0200

    test for parameters

--- tl.py
+++ tl.py
@@ -1,27 +1,63 @@
 #!/usr/bin/python
 import tempfile
 import subprocess
+import sys
 import os
 from os import path
+import argparse
 
-def convert(sourceDir, destDir, options):
-	for files in os.listdir(sourceDir):
-		if files.endswith(".jpg") or files.endswith(".jpeg"):
-			convertSingle(files, os.path.join(destDir, files), options)
+class TimeLapseCatchin:
+	def __init__(self):
+		self.sourceDir = "."
 
-def convertSingle(img, outimg, options):
-	args = ["convert"]
-	args.extend(options)
-	args.extend([img, outimg])
-	subprocess.call(args)
+	def convertAll(self, sourceDir, destDir, options):
+		for files in os.listdir(sourceDir):
+			if files.endswith(".jpg") or files.endswith(".jpeg"):
+				self.convertSingle(files, os.path.join(destDir, files), options)
 
-def testConvert(img, options):
-	tmpfile = tempfile.NamedTemporaryFile(suffix=".jpg")
-	convertSingle(img, tmpfile.name, options)
-	subprocess.call(["eog", tmpfile.name])
-	print(tmpfile.name)
-	tmpfile.close()
+	def convertSingle(self, img, outimg, options):
+		args = ["convert"]
+		args.extend(options)
+		args.extend([img, outimg])
+		return subprocess.call(args)
 
-#testConvert("/data/dateien/Bilder/Digicam/2012/07_Stuttgart/6/20120730-n032732.jpg", ["-scale", "1920x1080"])
+	def testForConvertParameters(self, options=None):
+		print("Enter the conversion you want to do.\n" \
+				"Examples:\n" \
+				" -crop 2144x1206+0+109 +repage -scale 1920x1080\n" \
+				" -crop 2144x1206+0+109 +repage -scale 1920x1080 -level 3,95\n" \
+				" -rotate 2.0 -crop 2087x1174+42+84 +repage -scale 1920x1080\n" \
+				" -crop 3216x1809+0+50 +repage -scale 1920x1080\n\n" \
+				"Enter a blank line when you are satisfied with the output image.\n")
+
+		while True:
+			sys.stdout.write("=> ")
+			readline = sys.stdin.readline().strip()
+			if len(readline) == 0:
+				return options
+			options = readline.split(" ")
+			self.testConvert("20120730-n032732.jpg", options)
+
+	def testConvert(self, img, options):
+		tmpfile = tempfile.NamedTemporaryFile(suffix=".jpg")
+		success = self.convertSingle(os.path.join(self.sourceDir,img), tmpfile.name, options)
+		if success == 0:
+			subprocess.call(["eog", tmpfile.name])
+		tmpfile.close()
+	
+	def run(self):
+		options = self.testForConvertParameters()
+		print(options)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--source", type=str, help="directory where all time lapse images are")
+args = parser.parse_args()
+
+tl = TimeLapseCatchin()
+if args.source:
+	tl.sourceDir = args.source
+
+tl.run()
 
 
