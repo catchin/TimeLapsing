bf71bac8b4931d44b2b0e16f043100bfd6d9b324
Fabian Kneißl <fabian@kneissl-web.net>
2012-08-06 20:20:30 +0200

    version kind of ready

--- tl.py
+++ tl.py
@@ -7,8 +7,16 @@ from os import path
 import argparse
 
 class TimeLapsing:
-	def __init__(self):
-		self.sourceDir = "."
+	def __init__(self, args):
+		if not args.convert and not args.movie:
+			self.isConvertImages = True
+			self.isCreateMovie = True
+		else:
+			self.isConvertImages = args.convert
+			self.isCreateMovie = args.movie
+		self.isHQMovie = args.hq
+		self.sourceDir = args.source
+		self.overwrite = args.overwrite
 		self.configDir = "~/.config/timelapsing"
 		self.options = None
 		self.fileSequenceName = "filesequence.txt"
@@ -20,8 +28,14 @@ class TimeLapsing:
 	def convertAll(self):
 		files = self.allFiles()
 		sys.stdout.write("Converting %s images" % len(files))
+		overwrite = None
 		for f in files:
-			self.convertSingle(f, os.path.join(self.destDir, f))
+			destFile = os.path.join(self.destDir, f)
+			if not os.path.exists(destFile) or self.overwrite or overwrite:
+				self.convertSingle(f, destFile)
+			elif overwrite == None:
+				sys.stdout.write("\n")
+				overwrite = self.confirm("Overwrite existing images")
 			sys.stdout.write(".")
 			sys.stdout.flush()
 		print("done")
@@ -64,37 +78,66 @@ class TimeLapsing:
 	def createImageSequence(self):
 		files = self.allFiles()
 		files.sort()
-		with open(os.path.join(self.sourceDir, self.fileSequenceName), "w") as f:
-			f.writelines(files)
+		filename = os.path.join(self.sourceDir, self.fileSequenceName)
+		if self.confirm("Overwrite image sequence file", filename):
+			with open(filename, "w") as f:
+				f.writelines(map(lambda s: os.path.join(self.destDir, s) + "\n", files))
 	
-	def createMovie(self, hq=False):
-		args = ["mencoder"]
-		if hq:
-			args.extend(["-ovc", "x264", "-x264encopts", "preset=veryslow:tune=film:crf=15:frameref=15:fast_pskip=0:threads=auto"])
+	def createMovie(self):
+		filename = os.path.join(self.sourceDir, self.movieName)
+		if self.confirm("Overwrite time lapse movie", filename=filename):
+			args = ["mencoder"]
+			if self.isHQMovie:
+				args.extend(["-ovc", "x264", "-x264encopts", "preset=veryslow:tune=film:crf=15:frameref=15:fast_pskip=0:threads=auto"])
+			else:
+				args.extend(["-ovc", "lavc", "-lavcopts", "vcodec=mpeg4:mbd=2:trell:autoaspect:vqscale=3"])
+			args.extend(["-nosound", 
+					"-mf", "type=jpeg:fps=20",
+					"-o", filename, 
+					"mf://@%s" % os.path.join(self.sourceDir, self.fileSequenceName)])
+			subprocess.call(args)
+	
+	def confirm(self, prompt, defaultResponse=False, filename=None):
+		if filename:
+			if not os.path.exists(filename) or self.overwrite:
+				return True
+			else:
+				prompt = prompt + " " + filename
+		if defaultResponse:
+			prompt = prompt + " [Y/n]?"
 		else:
-			args.extend(["-ovc", "lavc", "-lavcopts", "vcodec=mpeg4:mbd=2:trell:autoaspect:vqscale=3"])
-		args.extend(["-nosound", 
-				"-mf", "type=jpeg:fps=20",
-				"-o", self.movieName, 
-				"mf://@%s" % self.fileSequenceName])
-		subprocess.call(args)
+			prompt = prompt + " [y/N]?"
+		while True:
+			answer = raw_input(prompt)
+			if not answer:
+				return defaultResponse
+			elif answer.lower() == "y" or answer.lower() == "j":
+				return True
+			elif answer.lower() == "n":
+				return False
 	
 	def run(self):
-		self.testForConvertParameters()
 		self.destDir = os.path.join(self.sourceDir, "converted")
-		os.mkdir(self.destDir)
-		self.convertAll()
-		self.createImageSequence()
-		self.createMovie()
+		if not os.path.isdir(self.destDir):
+			os.mkdir(self.destDir)
+		if self.isConvertImages:
+			self.testForConvertParameters()
+			self.convertAll()
+			self.createImageSequence()
+		if self.isCreateMovie:
+			self.createMovie()
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-s", "--source", type=str, help="directory where all time lapse images are")
+parser.add_argument("-o", "--overwrite", action="store_true", help="overwrite files without notice")
+parser.add_argument("-s", "--source", type=str, default=".", help="directory where all time lapse images are")
+parser.add_argument("-hq", action="store_true", help="create the movie in high quality (slower)")
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-c", "--convert", action="store_true", help="convert images and create sequence file")
+group.add_argument("-m", "--movie", action="store_true", help="create a movie from sequence file")
 args = parser.parse_args()
 
-tl = TimeLapsing()
-if args.source:
-	tl.sourceDir = args.source
+tl = TimeLapsing(args)
 
 tl.run()
 
